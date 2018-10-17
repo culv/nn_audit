21e1d8a2cf4a7a47ca96db041cb0302ea60c31fa
culver <culv@users.noreply.github.com>
2018-10-17 16:38:27 -0400

    method to evaluate dimensionality reduction on sheared MNIST

--- nn_audit_mnist_shear_experiment.py
+++ nn_audit_mnist_shear_experiment.py
@@ -52,8 +52,78 @@ parser.add_argument('--p', type=int, default=20,
 
 
 def prepare_query_point(query, shear):
-    """ """
+    """Pass in an image from the test set and a desired shear, and return the
+    fully-connected neuron activations of the sheared image"""
     pass
+
+
+def evaluate_dim_red(test_dataset, train_dataset, knn_model, dim_red_model):
+    """Evaluate the dimensionality reduction method for neural network auditing purposes. For each test
+    example, calculate the:
+    
+        1) percentage of nearest training neighbors that do not have the same class prediction as the 
+            test example
+        2) variance of nearest train set neighbor shears relative to test example shear
+
+    and average these two measures over the entire test set.
+
+    Args:
+        test_dataset = The HDF5 file of neuron activations for the sheared test set
+        train_dataset = The HDF5 file for sheared train set
+        knn_model = A kNN model fit to the training set neuron activations
+        dim_reduce = A valid dimensionality reduction object with a 'project' method (eg. PCA, LDA, etc.)
+            that is fit to the training set
+
+    Returns:
+        predict_err = The average percentage of nearest neighbor training predictions different from test
+            predictions
+        shear_var = The average variance of nearest neighbor training shears from test shears
+    """
+
+    # Initialize shear_var
+    predict_err = 0
+    shear_var = 0
+
+    # Length of test set
+    n_test = test_dataset['shears'].shape[0]
+
+    # Batch size for knn searches
+    bs = 512
+
+    # Number of batches
+    n_batches = np.ceil(n_test/bs).astype(np.uint)
+
+    # Loop over batches of shears and neuron activations
+    b_start = 0
+    for b in tqdm(range(n_batches)):
+        # Get end index of the batch
+        b_end = min(n_test, b_start+bs)
+
+        # Get batch of activations (and project them) and shears and predictions
+        a = dim_red_model.project(test_dataset['activations'][b_start:b_end,:])
+        s = np.array(test_dataset['shears'][b_start:b_end])
+        p = np.array(test_dataset['predictions'][b_start:b_end])
+
+        # Get nearest neighbors for neuron batch
+        neighbs, _ = knn_model.query(a)
+
+        # Get the shears corresponding to neighbors
+        s_neighbs = np.array(train_dataset['shears'])[neighbs]
+        p_neighbs = np.array(train_dataset['predictions'])[neighbs]
+
+        # Calculate batch prediction error and batch variance
+        b_err = np.mean(p_neighbs != p[:,np.newaxis])
+        b_var = np.mean((s_neighbs-s[:,np.newaxis])**2)
+
+        # Weight batch calculations and then add to totals
+        b_weight = (b_end-b_start)/n_test
+        predict_err += b_weight*b_err
+        shear_var += b_weight*b_var
+
+        # Increment batch start to begin from where previous batch ended
+        b_start = b_end
+
+    return predict_err, shear_var
 
 
 def main():
@@ -104,10 +174,15 @@ def main():
     fc = SimpleNN()
     fc.load_state_dict(torch.load('./models/shear_fc100_09.pt'))
 
+    ####### PART 1: Calculate knn shear variance and knn class error ###########################
 
-    ####### PART 1: Visualize histograms of randomly sheared test samples ########
+    PCA_predict_err, PCA_shear_var = evaluate_dim_red(test_neurons, train_neurons, knn, pca)
+    print('PCA predict err: ', PCA_predict_err)
+    print('PCA shear var: ', PCA_shear_var)
 
-    query_shear = 45.72
+    ####### PART 2: Visualize histograms of randomly sheared test samples ########
+
+    query_shear = 50
     test_set = datasets.MNIST('./datasets/mnist', train=False, download=True,
         transform=transforms.Compose([
             RandomChoiceShear([query_shear]),
@@ -116,7 +191,7 @@ def main():
             ]))    
 
     # Get a test sample
-    query_image, query_label = test_set[2406] 
+    query_image, query_label = test_set[2408] 
     query_image_display = 1.-((query_image*0.3081)+1.307).squeeze()
 
     # Get neuron activations and prediction
@@ -134,7 +209,7 @@ def main():
     neighbor_shears = np.array([train_neurons['shears'][i] for i in neighbs])
 
     # Get a second query image with a different shear
-    query_shear2 = -17.34
+    query_shear2 = -10
     test_set2 = datasets.MNIST('./datasets/mnist', train=False, download=True,
         transform=transforms.Compose([
             RandomChoiceShear([query_shear2]),
